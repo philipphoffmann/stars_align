0d87d8a2c79f77bab11f77f84ba1293d9c8eacac
Philipp Hoffmann <mail@philipphoffmann.de>
2019-02-12 08:26:23 +0100

    Some minor refactorings

--- stars_align.py
+++ stars_align.py
@@ -1,5 +1,6 @@
 import logging
 import re
+from typing import NamedTuple
 
 from PIL import Image
 import pytesseract
@@ -9,54 +10,45 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 
-class Star:
-    def __init__(self, pos_x: int, pos_y: int, velocity_x: int, velocity_y: int):
-        self.pos_x = pos_x
-        self.pos_y = pos_y
-        self.velocity_x = velocity_x
-        self.velocity_y = velocity_y
+class Point(NamedTuple):
+    x: int
+    y: int
 
-    def __repr__(self):
-        return "<" + str(self.pos_x) + ", " + str(self.pos_y) + "><" + str(self.velocity_x) + ", " + str(
-            self.velocity_y) + ">"
+    def __add__(self, other) -> 'Point':
+        return Point(self.x + other.x, self.y + other.y)
+
+
+class Star(NamedTuple):
+    pos: Point
+    vel: Point
+
+    @classmethod
+    def from_string(cls, string: str) -> 'Star':
+        match = re.search("position=<\s*(-?\d+),\s*(-?\d+)> velocity=<\s*(-?\d+),\s*(-?\d+)>", string)
+        return Star(Point(int(match.group(1)), int(match.group(2))), Point(int(match.group(3)), int(match.group(4))))
 
 
 def parse_input(file: str) -> [Star]:
-    stars = []
     with open(file) as f:
-        for line in f:
-            line = line.replace("\n", "")
-            logger.debug("read: %s", line)
-            match = re.search("position=<\s*(-?\d+),\s*(-?\d+)> velocity=<\s*(-?\d+),\s*(-?\d+)>", line)
-            if match:
-                stars.append(Star(
-                    int(match.group(1)),
-                    int(match.group(2)),
-                    int(match.group(3)),
-                    int(match.group(4))
-                ))
-            else:
-                logger.warning("Unable to parse line: '%s'", line)
-
-    return stars
+        return [Star.from_string(line) for line in f]
 
 
 def move_stars(stars: [Star]) -> [Star]:
-    return [Star(star.pos_x + star.velocity_x, star.pos_y + star.velocity_y, star.velocity_x, star.velocity_y) for star in stars]
+    return [Star(star.pos + star.vel, star.vel) for star in stars]
 
 
 stars = parse_input("input.txt")
-additional_img_border = 3 # we need some border around the text, otherwise tesseract will not recognize the text
+additional_img_border = 3  # we need some border around the text, otherwise tesseract will not recognize the text
 seconds_passed = 0
 
 while True:
     seconds_passed += 1
     stars = move_stars(stars)
 
-    min_x = min([star.pos_x for star in stars])
-    min_y = min([star.pos_y for star in stars])
-    max_x = max([star.pos_x for star in stars])
-    max_y = max([star.pos_y for star in stars])
+    min_x = min(star.pos.x for star in stars)
+    min_y = min(star.pos.y for star in stars)
+    max_x = max(star.pos.x for star in stars)
+    max_y = max(star.pos.y for star in stars)
 
     image_width = (max_x - min_x) + (2 * additional_img_border)
     image_height = (max_y - min_y) + (2 * additional_img_border)
@@ -66,7 +58,7 @@ while True:
         logger.debug("Rendering frame (%s, %s) ...", image_width, image_height)
         frame = Image.new("1", (image_width, image_height), color=255)
         for star in stars:
-            frame.putpixel((star.pos_x - min_x + additional_img_border, star.pos_y - min_y + additional_img_border), 0)
+            frame.putpixel((star.pos.x - min_x + additional_img_border, star.pos.y - min_y + additional_img_border), 0)
         frame.save("frame.png")
         logger.debug("... done")
 
